b8db3c2a67c5192cc290a784f800c8917b83858a
fabiooo4 <fabioirimie@gmail.com>
2025-06-15 15:58:58 +0200

    feat: Basic 2d image rendering and resizing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kitty render
+This is a 3D renderer built in rust for the kitty terminal using kitty image protocol. The purpose is to build a basic rasterizer.
---
+++ src/main.rs
@@ -0,0 +1,190 @@
+use kitty_image::{Action, ActionTransmission, Command, Format, Medium, WrappedCommand};
+use nix::{
+    ioctl_read_bad,
+    libc::{self, winsize},
+};
+use std::{
+    borrow::Cow,
+    io::stdout,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    thread::sleep,
+    time::Duration,
+};
+
+fn main() {
+    // Setup CTRL + C handler
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    clearscreen::clear().unwrap();
+    while running.load(Ordering::SeqCst) {
+        let winsize = get_term_size();
+
+        let width: usize = winsize.ws_xpixel as usize;
+        let height: usize = winsize.ws_ypixel as usize;
+
+        let mut img_buf: Vec<Vec<Color>> =
+            vec![vec![Color::try_from("#000000").unwrap(); width]; height];
+
+        (0..height).for_each(|y| {
+            (0..width).for_each(|x| {
+                let red: f32 = x as f32 / (width as f32 - 1.);
+                let green: f32 = y as f32 / (height as f32 - 1.);
+                img_buf[y][x] = Color::new((red * 255.) as u8, (green * 255.) as u8, 0, 0xFF);
+            });
+        });
+
+        // Create an action
+        let action = Action::TransmitAndDisplay(
+            ActionTransmission {
+                format: Format::Rgba32,
+                medium: Medium::Direct,
+                width: width as u32,
+                height: height as u32,
+                ..Default::default()
+            },
+            kitty_image::ActionPut {
+                ..Default::default()
+            },
+        );
+
+        // Add the payload to the command
+        let mut command = Command::new(action);
+        command.payload = buf_to_payload(&img_buf);
+
+        // Wrap the command in escape codes
+        let command = WrappedCommand::new(command);
+
+        command.send_chunked(&mut stdout()).unwrap();
+
+        sleep(Duration::from_millis(100));
+    }
+    sleep(Duration::from_millis(50));
+    clearscreen::clear().unwrap();
+}
+
+fn buf_to_payload(img_buf: &Vec<Vec<Color>>) -> Cow<[u8]> {
+    let mut payload: Vec<u8> = Vec::with_capacity(4 * img_buf.len());
+
+    for row in img_buf {
+        for color in row {
+            payload.push(color.red);
+            payload.push(color.green);
+            payload.push(color.blue);
+            payload.push(color.alpha);
+        }
+    }
+
+    Cow::Owned(payload)
+}
+
+fn get_term_size() -> winsize {
+    ioctl_read_bad!(tiocgwinsz, libc::TIOCGWINSZ, winsize);
+
+    unsafe {
+        let sz: *mut winsize = &mut winsize {
+            ws_row: 0,
+            ws_col: 0,
+            ws_xpixel: 0,
+            ws_ypixel: 0,
+        };
+        let _ = tiocgwinsz(0, sz);
+        *sz
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+    alpha: u8,
+}
+
+impl Color {
+    fn new(red: u8, green: u8, blue: u8, alpha: u8) -> Self {
+        Color {
+            red,
+            green,
+            blue,
+            alpha,
+        }
+    }
+}
+
+impl TryFrom<&str> for Color {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if (value.len() != 9 || value.len() != 7) && value.chars().nth(0) != Some('#') {
+            return Err(String::from("not a hex string"));
+        }
+
+        let bytes = match u32::from_str_radix(&value[1..], 16) {
+            Ok(bytes) => {
+                if value.len() == 7 {
+                    bytes << 8
+                } else {
+                    bytes
+                }
+            }
+
+            Err(e) => return Err(e.to_string()),
+        };
+
+        let red = ((bytes & 0xFF000000) >> (8 * 3)) as u8;
+        let green = ((bytes & 0x00FF0000) >> (8 * 2)) as u8;
+        let blue = ((bytes & 0x0000FF00) >> 8) as u8;
+        let mut alpha = 0xFF;
+
+        if value.len() == 9 {
+            alpha = (bytes & 0x000000FF) as u8;
+        }
+
+        Ok(Color::new(red, green, blue, alpha))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_color_from_str_noalpha() {
+        assert_eq!(
+            Color::try_from("#fce822"),
+            Ok(Color::new(0xfc, 0xe8, 0x22, 0xff))
+        )
+    }
+
+    #[test]
+    fn test_color_from_str_alpha() {
+        assert_eq!(
+            Color::try_from("#ab23ffa0"),
+            Ok(Color::new(0xab, 0x23, 0xff, 0xa0))
+        )
+    }
+
+    #[test]
+    fn test_color_from_str_parse_error() {
+        assert_eq!(
+            Color::try_from("#ab23ffag"),
+            Err(String::from("invalid digit found in string"))
+        )
+    }
+
+    #[test]
+    fn test_color_from_str_error() {
+        assert_eq!(
+            Color::try_from("ab23ffag"),
+            Err(String::from("not a hex string"))
+        )
+    }
+}
