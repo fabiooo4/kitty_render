141fba3c2f4ea9c72f9d2d5388d0b395f7c0b16f
fabiooo4 <fabioirimie@gmail.com>
2025-08-11 14:49:06 +0200

    feat: Handled screen clearing and exit with termion

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,33 @@ mod vector;
 use model::{Model, load_obj};
 use nix::libc::EXIT_SUCCESS;
 use screen::{Color, Screen};
+use std::io;
 use std::io::Write;
-use std::process::{ExitCode, exit};
-use std::thread;
-use std::{
-    io,
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
-    thread::sleep,
-    time::Duration,
-};
+use std::process::exit;
 use termion::event::Key;
+use termion::raw::RawTerminal;
 use termion::{input::TermRead, raw::IntoRawMode};
 use vector::transform::Transform;
 
 fn main() {
     // Terminal setup -------------------
     // Set terminal to raw mode to allow reading stdin one key at a time
-    let mut _stdout = io::stdout().into_raw_mode().unwrap();
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+    stdout.activate_raw_mode().unwrap();
+
     // Use asynchronous stdin
     let mut stdin = termion::async_stdin().keys();
 
-    clearscreen::clear().unwrap();
+    write!(
+        stdout,
+        "{}{}{}",
+        // Clear the screen.
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide
+    )
+    .unwrap();
+    stdout.flush().unwrap();
     // Terminal setup -------------------
 
     // Init -----------------------------
@@ -50,7 +53,7 @@ fn main() {
 
     // Loop -----------------------------
     loop {
-        handle_input(&mut stdin, &mut screen, &mut transform);
+        handle_input(&mut stdin, &mut stdout, &mut screen, &mut transform);
 
         screen.render(&cube, &transform);
 
@@ -61,6 +64,7 @@ fn main() {
 
 fn handle_input(
     stdin: &mut termion::input::Keys<termion::AsyncReader>,
+    stdout: &mut RawTerminal<io::Stdout>,
     screen: &mut Screen,
     transform: &mut Transform,
 ) {
@@ -72,6 +76,17 @@ fn handle_input(
         match key {
             // Exit if 'CTRL+C' is pressed
             Key::Ctrl('c') | Key::Esc => {
+                write!(
+                    stdout,
+                    "{}{}",
+                    termion::cursor::Show,
+                    termion::clear::AfterCursor
+                )
+                .unwrap();
+
+                stdout.suspend_raw_mode().unwrap();
+                stdout.flush().unwrap();
+
                 screen.delete_all_images();
                 exit(EXIT_SUCCESS);
             }
